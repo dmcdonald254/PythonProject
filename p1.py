e1ec7c717dfb3e2e5f099971c33da3425da9286e
Daniel McDonald <dmcdonald254@gmail.com>
2019-09-17 01:40:01 -0400

    Stupid changes

--- p1.py
+++ p1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import math
 import sys
 from os import rename
@@ -18,24 +17,3 @@ r = requests.get("https://www.google.com")
 print("Status Code: \n", r.status_code, " \n")
 print("Cookies: \n", r.cookies, "\n")
 print("Time elapsed: \n", r.elapsed, "\n")
-=======
-import math
-import sys
-from os import rename
-
-import requests
-
-print(sys.version) 
-print(sys.executable) 
-
-
-def greet(who_to_greet):
-    test = 'Test'
-    greeting =  'Hello, {}'.format(who_to_greet)
-    return greeting
-
-r = requests.get("https://www.google.com")
-print("Status Code: \n", r.status_code, " \n")
-print("Cookies: \n", r.cookies, "\n")
-print("Time elapsed: \n", r.elapsed, "\n\n")
->>>>>>> a0e11bbcbe3ae67e33e9a918e6004720742e08fd
