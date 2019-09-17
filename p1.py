f0635324910dfb0f30c003d81564b4818bdcb362
Daniel <48609441+dmcdonald254@users.noreply.github.com>
2019-09-17 00:32:25 -0400

    Initial Commit

---
+++ p1.py
@@ -0,0 +1,20 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version) 
+print(sys.executable) 
+
+
+def greet(who_to_greet):
+    test = 'Test'
+    greeting =  'Hello, {}'.format(who_to_greet)
+    return greeting
+
+r = requests.get("https://www.google.com")
+print("Status Code: \n", r.status_code, " \n")
+print("Cookies: \n", r.cookies, "\n")
+print("Time elapsed: \n", r.elapsed, "\n")
+print("History: \n", r.history, "\n")
